6578cbcf4aa8fc2b6971ab127189a1db91209ae8
manikanta-paramkusham <bablubablu23558@gmail.com>
2026-06-09 10:00:13 +0530

    Fix DB port

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 DB_CONFIG = {
     "host": os.environ.get("DB_HOST"),
-    "port": int(os.environ.get("DB_PORT", 3306)),
+    "port": int(os.environ.get("DB_PORT") or 3306),
     "user": os.environ.get("DB_USER"),
     "password": os.environ.get("DB_PASSWORD"),
     "database": os.environ.get("DB_NAME"),
